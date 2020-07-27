ecafc897e3b5d487df902dffc9eabfab414de210
Antonette Caldwell <pullmana8@gmail.com>
2020-07-27 16:00:12 -0500

    Setup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root "pages#index"
+
+  get "simple", to: "pages#simple"
+  get "no-router", to: "pages#no_router"
+
+  get "react-router(/*all)", to: "pages#index"
+
   resources :comments
-  
-  get 'hello_world', to: 'hello_world#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
