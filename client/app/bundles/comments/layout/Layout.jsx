d3312bcd1a73d777afdfe27a21a3d14f275424b5
Antonette Caldwell <pullmana8@gmail.com>
2020-07-27 16:02:12 -0500

    Set up layout

---
+++ client/app/bundles/comments/layout/Layout.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink as Link } from 'react-router-dom';
+
+import './Layout.scss';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class Layout extends Component {
+    static propTypes = {
+      children: PropTypes.object.isRequired,
+    };
+
+    /* eslint-disable react/no-unescaped-entities */
+    render() {
+      const { children } = this.props;
+      return (
+        <section>
+          <header>
+            <ul>
+              <li>
+                <Link exact to="/" activeClassName="active">
+                  Comments (Root URL)
+                </Link>
+              </li>
+              <li>
+                <Link to="/react-router" activeClassName="active">
+                  Test React Router ('/react-router')
+                </Link>
+              </li>
+              <li>
+                <Link to="/react-router/redirect" activeClassName="active">
+                  Test Redirect
+                  (url to '/react-router/redirect' which goes to root '/')
+                </Link>
+              </li>
+            </ul>
+          </header>
+          { children }
+          {/* eslint react/destructuring-assignment
+            {this.props.children} */}
+        </section>
+      );
+    }
+}
